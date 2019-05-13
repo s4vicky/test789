f6b22e9505cd5c1208e1d33fa6b4599829cb7517
s4vicky <s4vicky@hotmail.com>
2019-05-13 02:13:19 -0700

    Update appliation.java

--- test789/src/test/demo/vikram/appliation.java
+++ test789/src/test/demo/vikram/appliation.java
@@ -3,6 +3,6 @@ package test.demo.vikram;
 public class appliation {
  public static void main (String args[])
  {
-	 System.out.println("yahootest1233");
+	 System.out.println("yahootest456");
  }
 }
